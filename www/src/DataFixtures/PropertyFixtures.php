8474a2d8748e54c2ae692ee376b34113f4c0a450
dbroyer <dbroyer@sqli.com>
2024-11-12 11:03:02 +0100

    update button delete and fix css and fixtures

--- www/src/DataFixtures/PropertyFixtures.php
+++ www/src/DataFixtures/PropertyFixtures.php
@@ -21,10 +21,10 @@ class PropertyFixtures extends Fixture
         $faker = Factory::create('fr_FR');
         for ($i = 0; $i < 10; $i++) {
             $property = new property();
-            //Ce Faker va nous permettre d'alimenter l'instance de Season que l'on souhaite ajouter en base
+
             $property->setType($faker->randomElement(['Villa', 'Appartement', 'garage','place de parking']));
             $property->setName($faker->randomElement($nomOfProperty));
-            $property->setAddress($faker->address);
+            $property->setAddress($faker->streetName);
             $property->setBuilding($faker->name);
             $property->setEtage($faker->randomNumber(1,10));
             $property->setNumero($faker->randomNumber(1,10));
